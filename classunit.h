632b4494151b846c69d91bbe9eab584a8002ed9a
AnastasiaGolomazova <66026569+AnastasiaGolomazova@users.noreply.github.com>
2021-11-19 00:55:02 +0700

    ClassUnit

--- classunit.h
+++ classunit.h
@@ -1,11 +1,28 @@
 #ifndef CLASSUNIT_H
 #define CLASSUNIT_H
 
+#include "languageconst.h"
 
-class ClassUnit // позже разработать общее поведение для класса
-{
+class ClassUnit: public LanguageConst {
 public:
-    ClassUnit();
+    enum AccessModifier{
+        PUBLIC,
+        PROTECTED,
+        PRIVATE,
+        INTERNAL,
+        PROTECTED_INTERNAL,
+        PRIVATE_PROTECTED
+    };
+
+public:
+    explicit ClassUnit(const string &name, size_t fieldsSize): name(name), fields(fieldsSize) {}
+
+    virtual ~ClassUnit() = default;
+
+protected:
+    string name;
+    using Fields = vector<shared_ptr<LanguageConst>>;
+    vector<Fields> fields;
 };
 
 #endif
