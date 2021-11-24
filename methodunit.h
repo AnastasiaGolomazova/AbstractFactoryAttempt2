bbbd24022e960d1c8e8444ba5246f26846a96bdc
AnastasiaGolomazova <66026569+AnastasiaGolomazova@users.noreply.github.com>
2021-11-25 01:36:45 +0700

    Realization MetodUnit

--- methodunit.h
+++ methodunit.h
@@ -1,11 +1,27 @@
 #ifndef METHODUNIT_H
 #define METHODUNIT_H
 
+#include "languageconst.h"
 
-class MethodUnit // позже разработать общее поведение для метода
-{
+class MethodUnit : public LanguageConst {
 public:
-    MethodUnit();
+    enum Modifier{
+        STATIC = 1,
+        CONST = 1 << 1,
+        VIRTUAL = 1 << 2,
+        FINAL = 1 << 3
+    };
+
+public:
+    MethodUnit(const string &nameM, const string &returnTypeM, Flags flagsM): name(nameM), return_type(returnTypeM), flags(flagsM){}
+    virtual ~MethodUnit() = default;
+    void Add(const shared_ptr<LanguageConst> &languageConst, Flags /* flags */ = 0 );
+
+protected:
+    string name;
+    string returnType;
+    Flags flags;
+    vector<shared_ptr<LanguageConst>> body;
 };
 
 #endif
